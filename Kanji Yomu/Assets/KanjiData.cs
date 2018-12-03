a2f29948c441ef016bd11ec8458c772b902c5d31
wilfido <uirukun13@gmail.com>
2018-12-03 14:56:13 -0600

    Kanji
    AddedKanjiDataStorage

---
+++ Kanji Yomu/Assets/KanjiData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KanjiData : MonoBehaviour {
+
+    public string kanjiString = "一番";
+    public string romanjiString;
+    public string englishString;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
