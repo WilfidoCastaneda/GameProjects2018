f0329f11a72f9529f16d5fa4ca52db42dc08ce86
wilfido <uirukun13@gmail.com>
2018-12-03 15:06:27 -0600

    Kanji + Test Scene
    Updated the Kanji script so it has zero functions. Updated the scene

--- Kanji Yomu/Assets/KanjiData.cs
+++ Kanji Yomu/Assets/KanjiData.cs
@@ -5,16 +5,6 @@ using UnityEngine;
 public class KanjiData : MonoBehaviour {
 
     public string kanjiString = "一番";
-    public string romanjiString;
-    public string englishString;
-
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public string romanjiString = "ichiban";
+    public string englishString = "number one";
 }
