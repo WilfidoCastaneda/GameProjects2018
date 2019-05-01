3b33a532178726f36dbac21311e7285f6df8484c
Wilfido Castaneda <uirukun13@gmail.com>
2019-05-01 01:05:56 -0500

    Added a functioning input field
    player can now input text onto the screen

--- Kanji Yomu/Assets/GameObjects/Player/PlayerScript/PlayerController.cs
+++ Kanji Yomu/Assets/GameObjects/Player/PlayerScript/PlayerController.cs
@@ -1,16 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
+    // Use this for initialization
+    [SerializeField]
+    private UnityEngine.UI.InputField TextField;
+
+    [SerializeField]
+    private bool activeField;
+
 	void Start () {
-		
+        activeField = false;
+        TextField = GameObject.Find("Canvas").GetComponentInChildren<UnityEngine.UI.InputField>();
+        TextField.interactable = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        if (Input.GetButtonDown("Submit") == true)
+        {
+            if (activeField == false)
+            {
+                activeField = true;
+                TextField.interactable = true;
+                TextField.Select();
+            }
+
+            else
+            {
+                activeField = false;
+                TextField.interactable = false;
+                TextField.text = "";
+            }
+
+        }
 		
 	}
+
 }
