9b5442202c9e5f4e229d5eb4f869fd1d16f83731
Wilfido Castaneda <uirukun13@gmail.com>
2019-05-16 00:30:26 -0500

    Upgraded Art + Player
    Added some temporary art assets into the game. Added movement controls
    to player, updated projected text (color, size, transformation)

--- Kanji Yomu/Assets/GameObjects/Player/PlayerScript/PlayerController.cs
+++ Kanji Yomu/Assets/GameObjects/Player/PlayerScript/PlayerController.cs
@@ -7,21 +7,32 @@ using System;
 public class PlayerController : MonoBehaviour {
 
     // Use this for initialization
+    [Header("Pointers")]
     [SerializeField]
     public UnityEngine.UI.InputField TextField;
 
     [SerializeField]
     public EnemyListManager activeEnemyManager;
 
+    private Rigidbody2D myBody;
+
+    [Header("My Data")]
     [SerializeField]
     private bool activeField;
 
-	void Start () {
+    public float NormalSpeed;
+    public float SlowSpeed;
+    private float walkingSpeed;
+
+
+    void Start () {
         activeField = false;
         TextField = GameObject.Find("Canvas").GetComponentInChildren<UnityEngine.UI.InputField>();
         TextField.interactable = false;
         //activeEnemyManager = GameObject.Find("Enemy Manager").GetComponent<EnemyListManager>();
-	}
+        myBody = this.GetComponent<Rigidbody2D>();
+        walkingSpeed = NormalSpeed;
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -32,7 +43,9 @@ public class PlayerController : MonoBehaviour {
             {
                 activeField = true;
                 TextField.interactable = true;
+                TextField.ActivateInputField();
                 TextField.Select();
+                walkingSpeed = SlowSpeed;
             }
 
             else
@@ -41,7 +54,9 @@ public class PlayerController : MonoBehaviour {
                 activeField = false;
                 TextField.interactable = false;
                 activeEnemyManager.CheckList(TextField.text);
+                TextField.DeactivateInputField();
                 TextField.text = "";
+                walkingSpeed = NormalSpeed;
             }
 
         }
@@ -63,7 +78,16 @@ public class PlayerController : MonoBehaviour {
         {
             SceneManager.LoadScene(1);
         }
-		
-	}
+
+        if (Input.GetButton("Horizontal"))
+        {
+            myBody.AddForce(new Vector2(Input.GetAxis("Horizontal") * walkingSpeed, 0));
+        }
+        if (Input.GetButton("Vertical"))
+        {
+            myBody.AddForce(new Vector2(0 , Input.GetAxis("Vertical") * walkingSpeed));
+        }
+
+    }
 
 }
